202d015d957aa505eb5873b5a6c7b05cd6062077
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-11-30 12:49:37 -0800

    Added timer for how long scripts took to run

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 
 pub trait AdventDay {
     fn part1(&self, input:&str) -> String;
@@ -7,7 +8,12 @@ pub trait AdventDay {
         let input = std::fs::read_to_string(input_path)
             .expect("Failed to read input file");
         
-        println!("Part 1: {}", self.part1(&input)); 
-        println!("Part 2: {}", self.part2(&input));
+        let start = Instant::now();
+        let p1 = self.part1(&input);
+        println!("Part 1: {} ({:?})", p1, start.elapsed().as_millis()); 
+        
+        let start = Instant::now();
+        let p2 = self.part2(&input);
+        println!("Part 2: {} ({:?})", p2, start.elapsed().as_millis()); 
     }
 }
