be35ae6000d570663401b7eb658e4ff238f73c52
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-12-01 09:50:58 -0800

    Cleaning up code

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -20,34 +20,34 @@ impl Direction {
 }
 
 impl AdventDay for Day01 {
+    /// # Test
+    /// ```rust
+    /// let result = my_crate::add(2, 3);
+    /// assert_eq!(result, 5);
+    /// ```
     fn part1(&self, input: &str) -> String {
         let mut counter : u32 = 0;
         let mut dial : i32 = 50;
 
         for line in input.lines() {
             let mut chars = line.chars();
-            let first_char = match chars.next() { // Consume the first character
-                Some(c) => c,
-                None => continue, // Skip empty lines
+            let first_char = if let Some(c) = chars.next() {
+                c // consume the first character, removed from chars
+            } else {
+                continue; // Skip empty lines
             };
             let dir = Direction::from_char(first_char);
             // Make the rest a string and then i32
             let number : i32 = chars.collect::<String>().parse().unwrap();
-            match dir {
-                Direction::Left => {
-                    dial -= number;
-                },
-                Direction::Right => {
-                    dial += number;
-                }
-            }
-            let wrapped = ((dial % 100) + 100) % 100;
-            if wrapped == 0 {
-                counter += 1;
-            }
-            dial = wrapped;
+            let addendum = match dir {
+                Direction::Left => -number,
+                Direction::Right => number,
+            };
+            dial += addendum;
+            dial = dial.rem_euclid(100);
+            counter += if dial == 0 { 1 } else { 0 };
         }
-        
+        // Last line is same as return
         counter.to_string()
     }
     fn part2(&self, input: &str) -> String {
